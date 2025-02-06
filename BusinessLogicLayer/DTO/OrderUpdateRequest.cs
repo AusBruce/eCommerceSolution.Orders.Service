60e8ff50404428fcc92711c8e59dbca0ddea9bc0
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-02-06 18:08:17 +1100

    Create DTO class and IOrderService

---
+++ BusinessLogicLayer/DTO/OrderUpdateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerce.OrdersMicroservice.BusinessLogicLayer.DTO;
+
+public record OrderUpdateRequest(Guid OrderID, Guid UserID, DateTime OrderDate, List<OrderItemAddRequest> OrderItems)
+{
+    public OrderUpdateRequest() : this(default, default, default, default)
+    {
+    }
+}
